9096d4fa780ab4781c290a3fa71f3c6e7ed95256
CharlesT30 <106152769+CharlesT30@users.noreply.github.com>
2024-09-12 17:02:26 +0800

    try 2 (index)

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +10,22 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>MAANGAS</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <Image
+          source={{ uri: "https://i.ytimg.com/vi/2GNY4sDK5hY/mqdefault.jpg" }}
+          style={{ width: 500, height: 500, marginRight: 20 }}
+        />
+        <Image
+          source={{ uri: "https://pbs.twimg.com/ext_tw_video_thumb/1792372179990102017/pu/img/QG91FrbxbOjzQONO.jpg" }}
+          style={{ width: 500, height: 500 }}
+        />
+      </View>
+      <h2>Tngina ni Onecent</h2>
     </View>
   );
 }
