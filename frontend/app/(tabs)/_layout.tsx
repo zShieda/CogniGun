68cf9fa51b22b6c6227675a397c9656fd4edb56c
CharlesT30 <106152769+CharlesT30@users.noreply.github.com>
2024-09-23 21:07:01 +0800

    home.txt (sidebar)

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,14 +1,17 @@
-import { Tabs } from "expo-router"
+import { Tabs } from "expo-router";
 
 export default () => {
-    return(
-        <Tabs>
-             <Tabs.Screen
+  return (
+    <Tabs>
+      <Tabs.Screen
         name="scan"
         options={{ headerShown: false, tabBarStyle: { display: 'none' } }}
       />
-            <Tabs.Screen name="pin"/>
-            <Tabs.Screen name="home"/>
-        </Tabs>
-    )
-}
+
+      <Tabs.Screen
+        name="home"
+        options={{ headerShown: false }}
+      />
+    </Tabs>
+  );
+};
